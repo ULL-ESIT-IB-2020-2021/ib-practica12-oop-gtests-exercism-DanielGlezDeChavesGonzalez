e2a699bf0ae0f8d584bedf05f59ade28e1c7b3a2
usuario <alu0101407951@ull.edu.es>
2021-01-10 14:37:14 +0000

    gtest

--- complejo/src/complejos_test.cc
+++ complejo/src/complejos_test.cc
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include "funciones_complejo.h"
 
-TEST(CalcTest, operator<) {
+TEST(Complejo, add) {
     ASSERT_EQ(2, add(1, 1));
     ASSERT_EQ(5, add(3, 2));
     ASSERT_EQ(10, add(7, 3));
 }
 
-TEST(CalcTest, Sub) {
+TEST(Complejo, sub) {
     ASSERT_EQ(3, sub(5, 2));
     ASSERT_EQ(-10, sub(5, 15));
 }
